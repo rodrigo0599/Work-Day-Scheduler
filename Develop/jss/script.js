d7fd5705a3988ef7dce304f9f27c93394c8c5480
rodrigo0599 <137252960+rodrigo0599@users.noreply.github.com>
2023-11-27 15:18:20 -0500

    1

--- Develop/jss/script.js
+++ Develop/jss/script.js
@@ -1,34 +1,67 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
+  if (typeof Storage === "undefined") {
+    alert(
+      "Local storage is not supported by this browser. Your data cannot be saved."
+    );
+    $(".saveBtn").prop("disabled", true);
+    return;
+  }
+
+  function saveToLocalStorage(key, value) {
+    try {
+      localStorage.setItem(key, value);
+    } catch (e) {
+      alert("Error saving to local storage. Your data might not be saved.");
+      console.error("Local Storage write error: ", e.message);
+    }
+  }
+
   $(".saveBtn").on("click", function () {
     var timeBlockId = $(this).parent().attr("id");
     var userInput = $(this).siblings(".description").val();
-    localStorage.setItem(timeBlockId, userInput);
+    saveToLocalStorage(timeBlockId, userInput);
   });
 
   for (var i = 9; i < 18; i++) {
-    var timeBlockId = "#hour-" + i;
-    var savedUserInput = localStorage.getItem(timeBlockId);
-    $(timeBlockId).children(".description").val(savedUserInput);
-  }
-  // Add code to color-code each time-block based on whether it is in the past,
-  // present, or future based on the current time. HINT: How can the current
-  // time be compared to the time represented by each time-block?
-  var currentHour = dayjs().hour();
-  $(".time-block").each(function () {
-    var timeBlockHour = parseInt($(this).attr("id").split("-")[1]);
-    if (timeBlockHour < currentHour) {
-      $(this).addClass("past");
-    } else if (timeBlockHour === currentHour) {
-      $(this).addClass("present");
-    } else {
-      $(this).addClass("future");
+    var timeBlockId = "hour-" + i;
+    try {
+      var savedUserInput = localStorage.getItem(timeBlockId);
+      if (savedUserInput) {
+        $("#" + timeBlockId)
+          .children(".description")
+          .val(savedUserInput);
+      }
+    } catch (e) {
+      console.error("Error reading from local storage: ", e.message);
     }
-  });
+  }
 
-  // Add code to display the current date in the header of the page.
-  var currentDate = dayjs().format("dddd, MMMM D, YYYY, [at] hh:mm:ss A");
-  $("#currentDay").text(currentDate);
+  if (typeof dayjs === "undefined") {
+    alert("Day.js library is not loaded. Time-based features will not work.");
+    console.error("Day.js library is not loaded.");
+    return;
+  }
+
+  try {
+    var currentHour = dayjs().hour();
+    $(".time-block").each(function () {
+      var timeBlockHour = parseInt($(this).attr("id").split("-")[1]);
+      if (timeBlockHour < currentHour) {
+        $(this).addClass("past");
+      } else if (timeBlockHour === currentHour) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future");
+      }
+    });
+  } catch (e) {
+    console.error("Error with time-based color coding: ", e.message);
+  }
+
+  try {
+    var currentDate = dayjs().format("dddd, MMMM D, YYYY, [at] hh:mm:ss A");
+    $("#currentDay").text(currentDate);
+  } catch (e) {
+    console.error("Error displaying current date: ", e.message);
+  }
 });
